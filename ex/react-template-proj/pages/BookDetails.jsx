78545effcffd08fbd8ca915761da1436bb23a3cf
itamarsa <itamarr87@gmail.com>
2023-09-03 23:00:29 +0300

    add improvements to details section

--- ex/react-template-proj/pages/BookDetails.jsx
+++ ex/react-template-proj/pages/BookDetails.jsx
@@ -3,22 +3,49 @@ import { bookService } from "../services/books.service.js";
 
 const { useState, useEffect } = React
 
-export function BookDetails({bookId, onBack}) {
+export function BookDetails({ bookId, onBack }) {
 
     const [book, setBook] = useState(null)
 
-    useEffect(()=>{
+    useEffect(() => {
         bookService.get(bookId).then(setBook)
     }, [])
+
+    function pageCount(book) {
+        if (book.pageCount > 500) return 'Serious Reading'
+        if (book.pageCount > 200) return 'Descent Reading'
+        if (book.pageCount < 100) return 'Light Reading'
+    }
+
+    function bookAge(book) {
+        if (book.publishedDate < 1) return 'New'
+        if (book.publishedDate > 10) return 'Vintage'
+    }
+
+    function getPriceColor(book) {
+        if (book.listPrice['amount'] > 150) return 'red'
+        if (book.listPrice['amount'] < 20) return 'green'
+    }
+
+    function onSale(book) {
+        if (book.listPrice['isOnSale']) return 'On Sale!'
+    }
 
     if (!book) return <div>Loading...</div>
     return (
         <section className="book-details">
-            <h1>Book Title: {book.title}</h1>
-            <h1>Book Price: {book.listPrice['amount']}</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                 Enim rem accusantium, itaque ut voluptates quo? Vitae animi maiores nisi,
-                  assumenda molestias odit provident quaerat accusamus, reprehenderit impedit, possimus est ad?</p>
+            <h3>Book Title: {book.title}</h3>
+            <h1>Book Subtitle: {book.subtitle}</h1>
+            <h4>Book Authors: {book.authors}</h4>
+            <h4>Published Date: {book.publishedDate}</h4>
+            <h4>Book Age: {bookAge(book)}</h4>
+            <h4>Page Count: {book.pageCount}</h4>
+            <h4>Read Level: {pageCount(book)}</h4>
+            <h4>Book Category: {[book.categories]}</h4>
+            <h4 className={getPriceColor(book)}>Book Price: {book.listPrice['amount']}</h4>
+            <h4>Discount: {onSale(book)}</h4>
+            <h4>Book language: {book.language}</h4>
+            <p>Book Description :{book.description}</p>
             <button onClick={onBack}>Back</button>
         </section>
     )
